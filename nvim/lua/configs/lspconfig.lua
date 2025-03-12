c6b956266277bd4ffe40bd5c71778bd72a21e20c
Miguel Alves <miguel.alves.dev@protonmail.com>
2025-03-12 19:06:52 +0000

    nvchad + go

---
+++ nvim/lua/configs/lspconfig.lua
@@ -0,0 +1,21 @@
+local nvlsp = require "nvchad.configs.lspconfig"
+local lspconfig = require "lspconfig"
+
+nvlsp.defaults()
+
+lspconfig.gopls.setup {
+  on_attach = nvlsp.on_attach,
+  capabilities = nvlsp.capabilities,
+  cmd = { "gopls" },
+  filetypes = { "go", "gomod", "gowork", "gotmpl" },
+  root_dir = lspconfig.util.root_pattern("go.work", "go.mod", ".git"),
+  settings = {
+    gopls = {
+      completeUnimported = true,
+      usePlaceholders = true,
+      analyses = {
+        unusedparams = true,
+      },
+    },
+  },
+}
