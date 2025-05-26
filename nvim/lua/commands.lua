4986a66daba084cd14f069089d2055d8ae357c10
Miguel Alves <miguel.alves.dev@protonmail.com>
2025-05-26 20:38:43 +0100

    custom command for running go package tests

--- nvim/lua/commands.lua
+++ nvim/lua/commands.lua
@@ -1,3 +1,15 @@
 vim.api.nvim_create_user_command("MasonInstallAll", function()
     vim.cmd "MasonInstall lua-language-server stylua gopls goimports terraform-ls markdown-oxide"
 end, {})
+
+vim.api.nvim_create_user_command("GoTest", function()
+    if not os.getenv "TMUX" then
+        vim.notify("Not running inside tmux!", vim.log.levels.WARN)
+    end
+
+    local cwd = vim.fn.expand "%:p:h"
+    local test_cmd = "go test ./..."
+
+    local tmux_cmd = string.format("tmux split-window -v -c %q '%s; read'", cwd, test_cmd)
+    vim.fn.system(tmux_cmd)
+end, {})
